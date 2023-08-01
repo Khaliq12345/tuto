d71fe055c1c27ab103e55e1e186f4c740797c1a6
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-08-01 20:25:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import sync_playwright
 import streamlit as st
 import os
+os.system('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb')
+os.system('sudo dpkg -i google-chrome-stable_current_amd64.deb')
 
 
 with sync_playwright() as p:
