969c04c558079700379bb5fa8fe03138e24d4d51
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-05-17 21:57:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-from playwright.sync_api import sync_playwright
+import hrequests
 import streamlit as st
-import os
-os.system('playwright install')
 
-
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("http://playwright.dev")
-    st.write(page.title())
-    browser.close()
+st.write("Good")
